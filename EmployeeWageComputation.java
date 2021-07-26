8e6e40667bf923b7362c18b623cef7bf996b1733
vismayatk002 <vismayatk002@gmail.com>
2021-07-26 16:53:29 +0530

    Replaced if-else using Switch Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,30 +2,31 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
 
-        int ispresent=1;
+        final int isPresent=1;
+        final int fullTime=1;
         int wagePerHr=20;
         int partimeHr=8;
         int fullDayHr=16;
-        int fullTime=1;
         int dailyEmpWage;
-        double empCheck = Math.floor (Math.random() * 10) % 2;
+        int empCheck = (int)(Math.random() * 10) % 2;
         //check employee is present or absent
-        if (empCheck == ispresent){
-            double empType = Math.floor (Math.random() * 10) % 2;
-            dailyEmpWage=wagePerHr*fullDayHr;
-            //check employee is full time or part time
-            if(empType == fullTime){
-                System.out.println("Employee Type : Full Time ");
-                System.out.println("Employee Wage : "+ dailyEmpWage);
-            }
-            else{
-                System.out.println("Employee Type : Part Time ");
-                System.out.println("Employee Wage : "+ (dailyEmpWage/2));    
-            }
-        }
-        else{
-            System.out.println("Employee is Absent");
+        switch(empCheck){    
+            case isPresent  :    
+                int empType = (int)(Math.random() * 10) % 2;
+                dailyEmpWage=wagePerHr*fullDayHr;
+                //check employee type 
+                switch(empType){    
+                    case fullTime   : 
+                        System.out.println("Employee Type : Full Time ");
+                        System.out.println("Employee Wage : "+ dailyEmpWage); 
+                    break;
+                    default:
+                        System.out.println("Employee Type : Part Time ");
+                        System.out.println("Employee Wage : "+ (dailyEmpWage/2));
+                }              
+            break;   
+            default:
+                 System.out.println("Employee is Absent");
         }
     }
-
 }
