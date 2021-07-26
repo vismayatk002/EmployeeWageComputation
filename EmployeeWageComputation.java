49829cee884ba2c92e9cacc58aedbf7aaea49ee4
vismayatk002 <vismayatk002@gmail.com>
2021-07-26 16:04:11 +0530

    Added Part time Employee Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,16 +4,27 @@ public class EmployeeWageComputation {
 
         int ispresent=1;
         int wagePerHr=20;
-        int fullDayHr=8;
+        int partimeHr=8;
+        int fullDayHr=16;
+        int fullTime=1;
         int dailyEmpWage;
         double empCheck = Math.floor (Math.random() * 10) % 2;
-        
+        //check employee is present or absent
         if (empCheck == ispresent){
+            double empType = Math.floor (Math.random() * 10) % 2;
             dailyEmpWage=wagePerHr*fullDayHr;
-            System.out.println("Daily Wage ="+ dailyEmpWage);
+            //check employee is full time or part time
+            if(empType == fullTime){
+                System.out.println("Employee Type : Full Time ");
+                System.out.println("Employee Wage : "+ dailyEmpWage);
+            }
+            else{
+                System.out.println("Employee Type : Part Time ");
+                System.out.println("Employee Wage : "+ (dailyEmpWage/2));    
+            }
         }
         else{
-            System.out.println("employee is absent");
+            System.out.println("Employee is Absent");
         }
     }
 
