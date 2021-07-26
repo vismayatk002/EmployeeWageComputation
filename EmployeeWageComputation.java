06ec8732cfba788ba98c84d648d3724fe390a1ee
vismayatk002 <vismayatk002@gmail.com>
2021-07-26 18:10:42 +0530

    Added Monthly wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,22 +7,23 @@ public class EmployeeWageComputation {
         int wagePerHr=20;
         int partimeHr=8;
         int fullDayHr=16;
-        int dailyEmpWage;
+        int workingDays=20;     
+        int monthlyEmpWage;
         int empCheck = (int)(Math.random() * 10) % 2;
         //check employee is present or absent
         switch(empCheck){    
             case isPresent  :    
                 int empType = (int)(Math.random() * 10) % 2;
-                dailyEmpWage=wagePerHr*fullDayHr;
+                monthlyEmpWage= (wagePerHr*fullDayHr) * workingDays;
                 //check employee type 
                 switch(empType){    
                     case fullTime   : 
                         System.out.println("Employee Type : Full Time ");
-                        System.out.println("Employee Wage : "+ dailyEmpWage); 
+                        System.out.println("Employee Wage : "+ monthlyEmpWage); 
                     break;
                     default:
                         System.out.println("Employee Type : Part Time ");
-                        System.out.println("Employee Wage : "+ (dailyEmpWage/2));
+                        System.out.println("Employee Wage : "+ (monthlyEmpWage/2));
                 }              
             break;   
             default:
